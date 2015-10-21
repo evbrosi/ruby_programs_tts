0d9ab86d402fdd8cf158be239ae4fcccccd96f56
Eagle Brosi <evbrosi@gmail.com>
2015-10-21 10:31:50 -0400

    paired up students

--- day3/student_shuffle.rb
+++ day3/student_shuffle.rb
@@ -3,7 +3,19 @@
 
 students = %w(aaron john jane rolf ellen lucas jim book Smeagle)
 
-puts students
+students.shuffle!
+
+count = 0
+
+until count == (students.length - 1)
+
+	puts "#{students[count]} & #{students[count+1]}"
+
+	count += 2
+end
+
+
+
 
 
 
