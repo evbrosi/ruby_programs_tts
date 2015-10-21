9e1ff2e65dedd03e5a89a055bb07c9e27cd00216
Eagle Brosi <evbrosi@gmail.com>
2015-10-21 10:57:33 -0400

    program works

--- day3/student_shuffle.rb
+++ day3/student_shuffle.rb
@@ -1,15 +1,39 @@
 
 # %w (and names) puts them in a good space
 
-students = %w(aaron john jane rolf ellen lucas jim book Smeagle)
+# students = %w(aaron john jane rolf lucas jim book Smeagle)
+
+students = []
+complete = false
+
+puts "Who came to class today (type 'done' when finished)?"
+until complete == true
+	student =gets.chomp.downcase.capitalize
+	if student == "Done"
+		complete = true
+	else
+		students.push(student)
+	end
+end
+
 
 students.shuffle!
 
 count = 0
+num_students = students.length
 
-until count == (students.length - 1)
+until count == num_students
 
-	puts "#{students[count]} & #{students[count+1]}"
+	if num_students % 2 ==1
+		if count < num_students -3
+			puts "#{students[count]} & #{students[count+1]}"
+		else
+			puts "#{students[count]} & #{students[count+1]} & #{students[count+2]}"
+			break
+		end
+	else 
+		puts "#{students[count]} & #{students[count+1]}"
+	end
 
 	count += 2
 end
